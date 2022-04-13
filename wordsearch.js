dc4c5b6a12898052e1ee2ff3bae684155ce4095f
Baila3 <bailaly16@gmail.com>
2022-04-13 19:25:11 -0400

    test words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) 
+        return true
     }
+    const verticalJoin = []
+    for (let i = 0; i < letters[0].length; i++){
+      let string = ""
+      for( let y = 0; y < letters.length; y++){
+        string = string.concat(letters[y][i])
+      }
+      verticalJoin.push(string)
+      for (let t of verticalJoin){
+     if (t.includes(word)){
+     return true
+     }
+    }
+    }
+    return false
 }
 
 module.exports = wordSearch
